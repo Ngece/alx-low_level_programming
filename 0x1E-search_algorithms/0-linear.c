217d29756612bae6349309de975185996514c61d
Ngece <ngecemlamli@gmail.com>
2023-04-12 16:30:51 -0700

    committing again

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 
 // Defines the linear seach function
 int linear_search(int *array, size_t size, int value) {
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size - 1; i++) {
 		//print the elements checked and return i when found
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value) {
