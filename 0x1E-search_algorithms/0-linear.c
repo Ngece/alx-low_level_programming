2085973ca6e900194b7f4175bb6c574130c597a3
Ngece <ngecemlamli@gmail.com>
2023-04-12 17:12:25 -0700

    committing linear search again

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value) {
-    // Check if the array is NULL
-    if (array == NULL) {
-        return -1; // return -1 if array is NULL
-    }
-    // Loop through each element of the array
-    for (size_t i = 0; i < size; i++) {
-        // If the current element of the array matches the target value, return its index
-        if (array[i] == value) {
-            return i; // return index of first occurrence of value
-        }
-    }
-    // If the target value is not found in the array, return -1
-    return -1; // return -1 if value not found in array
-}
+	for (int i = 0; i < size; i++) {
+		//Print element checked until i mathches value
+		printf("Value checked array[%d] = [%d]", i, array[i])
+		if (array[i] == value) {
+			return 1;
+		}
+	}
+	return -1;
 
+}
